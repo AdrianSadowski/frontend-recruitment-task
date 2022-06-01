54ed53bc04b56f3fc241d278308f8b07973d2400
AdrianSadowski <sadow.adrian@gmail.com>
2022-06-01 11:50:02 +0200

    Add save data in localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,34 @@
   const closeButton = document.querySelector('#close-button');
   const overlay = document.querySelector('#overlay');
   const infoClick = document.querySelector('#pop-value');
+  const popup = document.querySelector('#popup-content');
   const resetButton = document.querySelector('#reset-button');
-  
+  let clickedResults = JSON.parse(localStorage.getItem('click'));
+
+  function getCountTimes(){
+    if (clickedResults == null) {
+      localStorage.setItem('click', JSON.stringify(0));
+    }
+    if(clickedResults >= 5){
+      resetButton.classList.add('active');
+      console.log('button');
+    }
+    infoClick.innerHTML = clickedResults;
+  }
 
   openButton.addEventListener('click', () => {
-    const popup = document.querySelector('#popup-content');
     openPopup(popup);
     addCountTimes();
   });
 
   closeButton.addEventListener('click', () => {
-    const popup = document.querySelector('#popup-content');
-    closePopup(popup);
+    closePopup();
   });
 
-
+  resetButton.addEventListener('click', () => {
+    localStorage.clear()
+    window.location.reload();
+  });
 
   function openPopup(popup) {
     if (popup == null) return;
@@ -26,11 +39,16 @@
     overlay.classList.add('active');
   }
 
-  function closePopup(popup) {
+  function closePopup() {
     if (popup == null) return;
     popup.classList.remove('active');
     overlay.classList.remove('active');
   }
 
+  function addCountTimes(){
+    localStorage.setItem('click', JSON.stringify(clickedResults++));
+    getCountTimes()
+  }
 
+  getCountTimes();
 }
